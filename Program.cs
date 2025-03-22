7a22c58a75ba7899a6211f7513fc3d8c4e0e37cf
Jose Carlos Cruz Florian <jcruzf6@miumg.edu.gt>
2025-03-21 18:36:37 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Herencia_Polimorfismo;
+
+
+Vehiculo miCarrito = new Vehiculo(2026, "Azul", "Alfa Romeo");
+
+Vehiculo elOtro = new Vehiculo(2000, "Rojo", "DELOREAN");
+
+CarroElectrico miBYD = new CarroElectrico(2026, "Amarillo", "BYD");
+
+AutoCombustión combus = new AutoCombustión(2025, "Rojo", "Ferrari");
+
+combus.InformacionVehiculo();
+combus.encender();
+combus.acelerar();
+combus.frenar();
+combus.frenar();
+//miBYD.cargarBateria();
+//Console.WriteLine("El nivel de bateria es: {0}", miBYD.NivelBateria());
+
